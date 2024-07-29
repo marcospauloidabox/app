8bc72587a690e0bdc58d2a4e0fc5223c8856dfd8
marcospaulosouza <marcospaulo@kirvano.com>
2024-07-28 23:56:40 -0300

    feat: add and config vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { createApp } from 'vue'
+
+// Vuetify
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Components
 import App from './App.vue'
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
