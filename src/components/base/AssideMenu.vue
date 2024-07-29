ca669688844ada4630220b65e2d99127fbcffe19
marcospaulosouza <marcospaulo@kirvano.com>
2024-07-29 07:28:54 -0300

    feat: create AssideMenu

---
+++ src/components/base/AssideMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="principal-menu">
+    <ul>
+      <li v-for="item in menu" :key="item.id">
+        <component :is="item.icon" />
+        <router-link :to="item.route">{{ item.name }}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, type Component } from 'vue'
+
+interface MenuItem {
+  id: number
+  name: string
+  icon: Component
+  route: string
+}
+
+const props = defineProps<{
+  menu: MenuItem[]
+}>()
+</script>
+
+<style scoped>
+.principal-menu {
+  font-size: large;
+  text-decoration: none;
+}
+.principal-menu ul {
+  list-style: none;
+  padding: 0;
+}
+</style>
--- src/components/layout/AssideLayout.vue
+++ src/components/layout/AssideLayout.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="asside-layout">
-    <h1>Asside</h1>
+    <menu-item :menu="menuItens" />
   </div>
 </template>
+<script>
+import Menu from '../base/AssideMenu.vue'
+import IconHome from '../icons/IconCommunity.vue'
+
+export default {
+  name: 'AssideLayout',
+  components: {
+    'menu-item': Menu,
+    'home': IconHome
+  },
+  data() {
+    return {
+      menuItens: [
+        { id: 1, name: 'Home', icon: 'home', route: '/' },
+        { id: 2, name: 'Products', icon: 'products', route: '/products' }
+      ]
+    }
+  }
+}
+</script>
 <style>
 .asside-layout {
   grid-area: A;
+}
+
+.asside-layout div {
+  padding: 30%;
 }
 </style>
